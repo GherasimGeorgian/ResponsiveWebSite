7bb999aafb2f3e07a748bf93ff04b2b4189a0050
Geo <Geo@DESKTOP-4HDFJN5>
2020-09-07 17:15:36 +0300

    Cart-Bind Price Data-Remove Item

--- ResponsiveWebSite/Cart.aspx
+++ ResponsiveWebSite/Cart.aspx
@@ -17,11 +17,11 @@
                         <div class="media-body">
                             <h5 class="media-heading proNameViewCart"><%#Eval("Name") %>
                             </h5>
-                            <p class="proPriceDiscountView">Size : <%#Eval("SizeNameP") %></p>
+                            <p class="proPriceDiscountView">Size : <%#Eval("SizeNamee") %></p>
                             <span class="proPriceView"><%#Eval("PSelPrice","{0:c}") %></span>
                             <span class="proOgPriceView"><%#Eval("PPrice","{0:0,00}") %></span>
                             <p>
-                                 <asp:Button ID="btnRemoveItem" runat="server" CssClass="removeButton" OnClick="btnRemoveItem_Click" Text="Remove" />
+                                 <asp:Button CommandArgument='<%#Eval("PID") + "-" + Eval("SizeIDD")%>' ID="btnRemoveItem" runat="server" CssClass="removeButton" OnClick="btnRemoveItem_Click" Text="Remove" />
                             </p>
                         </div>
                    
@@ -35,17 +35,17 @@
                 <h5 class="">Price details</h5>
                 <div>
                     <label>Cart total</label>
-                    <span class="pull-right priceGray">1898</span>
+                    <span class="pull-right priceGray" id="spanCartTotal" runat="server"></span>
                 </div>
                 <div>
                     <label>Cart discount</label>
-                    <span class="pull-right priceGreen">-232</span>
+                    <span class="pull-right priceGreen" id="spanDiscount" runat="server" ></span>
                 </div>
             </div>
             <div>
                 <div class="proPriceView">
                     <label>Total</label>
-                    <span class="pull-right"> rs:1400</span>
+                    <span class="pull-right" id="spanTotal" runat="server"></span>
                 </div>
                 <div>
                      <asp:Button ID="btnBuyNow" runat="server" CssClass="btnBuyNow" OnClick="btnBuyNow_Click" Text="BUY NOW" />
--- ResponsiveWebSite/Cart.aspx.cs
+++ ResponsiveWebSite/Cart.aspx.cs
@@ -26,50 +26,83 @@ public partial class Cart : System.Web.UI.Page
             string[] CookieDataArray = CookieData.Split(',');
             if (CookieDataArray.Length > 0)
             {
-                h5NoItems.InnerText = "My cart ("+ CookieDataArray.Length+" Items)";
+                h5NoItems.InnerText = "MY CART (" + CookieDataArray.Length + " Items)";
                 DataTable dt = new DataTable();
+                Int64 CartTotal = 0;
+                Int64 Total = 0;
                 for (int i = 0; i < CookieDataArray.Length; i++)
                 {
                     string PID = CookieDataArray[i].ToString().Split('-')[0];
                     string SizeID = CookieDataArray[i].ToString().Split('-')[1];
 
-                    String CS = ConfigurationManager.ConnectionStrings["MyDataBaseConnectionString1"].ConnectionString;
+                    String CS = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString1"].ConnectionString;
                     using (SqlConnection con = new SqlConnection(CS))
                     {
-                        using (SqlCommand cmd = new SqlCommand("select A.*,dbo.getSizeName("+SizeID+") as SizeNameP,"+SizeID+" as SizeIDD,SizeData.Name," +
-                            "SizeData.Extension  from tblProducts A cross apply(select top 1 B.Name,Extension from tblProductImages B where B.PID=A.PID)" +
-                            " SizeData where A.PID='" + PID + "'", con))
+                        using (SqlCommand cmd = new SqlCommand("select A.*,dbo.getSizeName(" + SizeID + ") as SizeNamee,"
+                            + SizeID + " as SizeIDD,SizeData.Name,SizeData.Extension from tblProducts A cross apply( select top 1 B.Name,Extension from tblProductImages B where B.PID=A.PID ) SizeData where A.PID="
+                            + PID + "", con))
                         {
                             cmd.CommandType = CommandType.Text;
                             using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
                             {
-                                
                                 sda.Fill(dt);
-                               
                             }
+
                         }
                     }
+                    CartTotal += Convert.ToInt64(dt.Rows[i]["PPrice"]);
+                    Total += Convert.ToInt64(dt.Rows[i]["PSelPrice"]);
                 }
                 rptrCartProducts.DataSource = dt;
                 rptrCartProducts.DataBind();
                 divPriceDetails.Visible = true;
+
+                spanCartTotal.InnerText = CartTotal.ToString();
+                spanTotal.InnerText = "Rs. " + Total.ToString();
+                spanDiscount.InnerText = "- " + (CartTotal - Total).ToString();
             }
             else
             {
+                //TODO Show Empty Cart
                 h5NoItems.InnerText = "Your Shopping Cart is Empty";
                 divPriceDetails.Visible = false;
             }
         }
         else
         {
+            //TODO Show Empty Cart
             h5NoItems.InnerText = "Your Shopping Cart is Empty";
             divPriceDetails.Visible = false;
+
         }
     }
 
     protected void btnRemoveItem_Click(object sender, EventArgs e)
     {
+         string CookiePID = Request.Cookies["CartPID"].Value.Split('=')[1];
+        Button btn = (Button)(sender);
+        string PIDSIZE = btn.CommandArgument;
 
+        List<String> CookiePIDList = CookiePID.Split(',').Select(i => i.Trim()).Where(i => i != string.Empty).ToList();
+        CookiePIDList.Remove(PIDSIZE);
+        string CookiePIDUpdated = String.Join(",", CookiePIDList.ToArray());
+        if (CookiePIDUpdated == "")
+        {
+            HttpCookie CartProducts = new HttpCookie("CartPID");
+            CartProducts.Values["CartPID"] = null;
+            CartProducts.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(CartProducts);
+
+        }
+        else
+        {
+            HttpCookie CartProducts = new HttpCookie("CartPID");
+            CartProducts.Values["CartPID"] = CookiePIDUpdated;
+            CartProducts.Expires = DateTime.Now.AddDays(30);
+            Response.Cookies.Add(CartProducts);
+
+        }
+        Response.Redirect("~/Cart.aspx");
     }
 
     protected void btnBuyNow_Click(object sender, EventArgs e)
